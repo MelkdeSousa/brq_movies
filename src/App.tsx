466b4355c95ff8b2daf57a8d8fec173ffc290328
Melk de Sousa <melk.sousap@gmail.com>
2023-11-23 19:34:38 -0300

    ✨ feat(android): set splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import {ThemeProvider} from 'styled-components/native';
 import {Login} from './screens/Login/Login';
 import {theme} from './styles/theme';
 
 export const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <Login />
