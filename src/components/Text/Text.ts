959d2d9d1847576c70267a36544a06a09c21671c
Melk de Sousa <melk.sousap@gmail.com>
2023-11-24 01:26:10 -0300

    Update ESLint config to disable react-in-jsx-scope rule & fix styled-components formatting inconsistency
    This commit updates the ESLint config to disable the
    'react/react-in-jsx-scope' rule and fixes the formatting inconsistency
    in the Text component in the 'src/components/Text/Text.ts' file,
    ensuring a consistent style.

--- src/components/Text/Text.ts
+++ src/components/Text/Text.ts
@@ -2,8 +2,8 @@ import { theme } from '@/styles/theme';
 import { fontTheme } from '@/utils/responsive';
 import styled from 'styled-components/native';
 
-export const Text = styled.Text<{ size?: keyof typeof theme.fontSize }>`
-  color: ${({ theme }) => theme.colors.secondary};
-  font-family: ${({ theme }) => theme.fontFamily.regular};
-  font-size: ${({ size = 'base' }) => fontTheme(size)};
+export const Text = styled.Text<{size?: keyof typeof theme.fontSize}>`
+  color: ${({theme}) => theme.colors.secondary};
+  font-family: ${({theme}) => theme.fontFamily.regular};
+  font-size: ${({size = 'base'}) => fontTheme(size)};
 `;
