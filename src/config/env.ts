b101b48e5a8d05b17fe711a4888f8df8e33af669
Melk de Sousa <melk.sousap@gmail.com>
2023-11-26 16:20:21 -0300

    Refactor environment configuration for better integration.
    Update .gitignore to ignore more environmental files. Refactor Babel
    configuration to use 'react-native-dotenv' module. Modify environment
    type definition and import in env.ts. Update type definition to use
    'react-native-dotenv' instead of '@env'.

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,8 +1,10 @@
-import { API_KEY_IMDB } from '@env';
+import { API_KEY_IMDB } from 'react-native-dotenv';
 import { z } from 'zod';
 
 const envSchema = z.object({
   API_KEY_IMDB: z.string(),
 });
+
+export type Env = z.infer<typeof envSchema>;
 
 export const env = envSchema.parse({ API_KEY_IMDB });
